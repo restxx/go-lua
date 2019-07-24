cde3c622fd3ff46dcd80ad98c40708b06bc758fb
restxx <doc007@163.com>
2019-07-25 01:34:50 +0800

    1111

--- src/ts_lua/person.lua
+++ src/ts_lua/person.lua
@@ -3,4 +3,10 @@ print(p:name()) -- "Steeve"
 
 p:name("Alice", "Alice2")
 
-p:buf({11,22,33,44,55,66,77,88})
+p:buf({11,22,33,44,55,66,77,88,99,10})
+
+print("-------------------------------------")
+
+for k, v in pairs(p:buf()) do
+    print(v)
+end
